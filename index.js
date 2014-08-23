39c82667089ccfda2fbbb1eb2e88fa309f503f4f
Jonathan Ong <jonathanrichardong@gmail.com>
2014-08-23 03:53:13 -0700

    rewrite in promises

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 
+var Promise = require('native-or-bluebird')
 var crypto = require('mz/crypto')
 var scmp = require('scmp')
 
@@ -17,27 +18,35 @@ Password.prototype.salt = function (length) {
   return crypto.randomBytes(length || this.length)
 }
 
-Password.prototype.hash = function* (password, salt, iterations, length) {
-  salt = salt || (yield this.salt()).toString('base64')
+Password.prototype.hash = function (password, salt, iterations, length) {
   iterations = iterations || this.iterations
   length = length || this.length
-  var hash = yield crypto.pbkdf2(password, salt, iterations, length)
-
-  return [
-    salt,
-    iterations,
-    length,
-    hash.toString('base64')
-  ].join(';')
+  return (salt ? Promise.resolve(salt) : this.salt().then(toBase64))
+    .then(function (_salt) {
+      return crypto.pbkdf2(password, salt = _salt, iterations, length)
+    })
+    .then(function (hash) {
+      return [
+        salt,
+        iterations,
+        length,
+        hash.toString('base64')
+      ].join(';')
+    })
 }
 
-Password.prototype.compare = function* (password, hash) {
+Password.prototype.compare = function (password, hash) {
   var frags = hash.split(';')
-  var res = yield* this.hash(
+  return this.hash(
     password,
     frags[0],
     parseInt(frags[1], 10),
     parseInt(frags[2], 10)
-  )
-  return scmp(hash, res)
+  ).then(function (res) {
+    return scmp(hash, res)
+  })
+}
+
+function toBase64(x) {
+  return x.toString('base64')
 }
--- test.js
+++ test.js
@@ -4,33 +4,33 @@ var pswd = require('./')()
 
 describe('pswd', function () {
   it('should hash a password', co(function* () {
-    var hash = yield* pswd.hash('password')
+    var hash = yield pswd.hash('password')
     hash.split(';').length.should.equal(4)
   }))
 
   it('should compare a password', co(function* () {
-    var hash = yield* pswd.hash('password')
-    var okay = yield* pswd.compare('password', hash)
+    var hash = yield pswd.hash('password')
+    var okay = yield pswd.compare('password', hash)
     okay.should.be.ok
   }))
 
   it('should reject an incorrect password', co(function* () {
-    var hash = yield* pswd.hash('password')
-    var okay = yield* pswd.compare('asdfadfs', hash)
+    var hash = yield pswd.hash('password')
+    var okay = yield pswd.compare('asdfadfs', hash)
     okay.should.not.be.ok
   }))
 
   it('should work when you change iterations', co(function* () {
-    var hash = yield* pswd.hash('password')
+    var hash = yield pswd.hash('password')
     pswd.iterations = 13000
-    var okay = yield* pswd.compare('password', hash)
+    var okay = yield pswd.compare('password', hash)
     okay.should.be.ok
   }))
 
   it('should work when you change length', co(function* () {
-    var hash = yield* pswd.hash('password')
+    var hash = yield pswd.hash('password')
     pswd.length = 64
-    var okay = yield* pswd.compare('password', hash)
+    var okay = yield pswd.compare('password', hash)
     okay.should.be.ok
   }))
 })
